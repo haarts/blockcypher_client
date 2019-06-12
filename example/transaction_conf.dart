d2ac93d01a3d6fc6241ffedee812f84768d3dee4
Harm Aarts <harm@aarts.email>
2019-06-12 09:42:40 +0200

    Switch example to testnet

--- example/transaction_conf.dart
+++ example/transaction_conf.dart
@@ -3,10 +3,10 @@ import 'package:blockcypher/blockcypher.dart';
 void main(List<String> args) async {
   var token = args[0];
   var client =
-      Client.websocket("wss://socket.blockcypher.com/v1/btc/main", token);
+      Client.websocket("wss://socket.blockcypher.com/v1/btc/test3", token);
 
   Stream<String> confs = await client
-      .transactionConfirmation("3ETUD57Zm1oKrGUikFLsh5xFRieWJzN1sc");
+      .transactionConfirmation("mv4rnyY3Su5gjcDNzbMLKBQkBicCtHUtFB");
   await for (String conf in confs) {
     print(conf);
   }
